a83bb4b82c842ad55197255c1affc10c746165ec
Fernando Ayats <ayatsfer@gmail.com>
2022-02-21 12:49:14 +0100

    channels-to-flakes

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -22,77 +21,67 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils-plus, ... }:
+  outputs =
+    inputs @
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
     let
-      nixosModules = flake-utils-plus.lib.exportModules (
-        nixpkgs.lib.mapAttrsToList (name: value: ./modules/${name}) (builtins.readDir ./modules)
-      );
-    in
-    flake-utils-plus.lib.mkFlake {
-      inherit self inputs nixosModules;
+      supportedSystems = ["x86_64-linux" "aarch64-linux"];
+    in {
+      lib = import ./lib { inherit (nixpkgs) lib; };
+      nixosModules = self.lib.exportModulesDir ./modules;
 
-      channelsConfig.allowUnfree = true;
+      nixosConfigurations.cloud = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit inputs;
+          pkgs = self.pkgs.${system};
+        };
+        modules = with self.nixosModules; [
+          common
+          channels-to-flakes
+          users
+          inputs.sops-nix.nixosModules.sops
+          sops
+          hardware-cloud
 
-      channels.nixpkgs.overlaysBuilder = channels: [
-        (final: prev: {
-          inherit (inputs.nixpkgs-unstable)
-          oci-cli
-          ;
-        })
-      ];
+          services
+          docker
+          drone
+          gitea
+          autoUpgrade
+        ];
+      };
 
-      hosts.cloud.modules = with self.nixosModules; [
-        common
-        users
-        inputs.sops-nix.nixosModules.sops
-        sops
-        hardware-cloud
-
-        services
-        docker
-        drone
-        gitea
-        autoUpgrade
-      ];
-
-      deploy.nodes = {
-        cloud = {
-          hostname = "cloud";
-          fastConnection = false;
-          profiles.system = {
-            sshUser = "admin";
-            path =
-              inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.cloud;
-            user = "root";
-          };
+      deploy.nodes.cloud = {
+        hostname = "cloud";
+        fastConnection = false;
+        profiles.system = {
+          sshUser = "admin";
+          path =
+            inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.cloud;
+          user = "root";
         };
       };
 
-      outputsBuilder = channels:
-        let
-          pkgs = channels.nixpkgs;
+      devShell = nixpkgs.lib.genAttrs supportedSystems (
+        system: let
+          pkgs = nixpkgs.legacyPackages.${system};
         in
-        {
-          devShell = channels.nixpkgs.mkShell {
-            name = "development-shell";
-            buildInputs =
-              let
-                make-vm = pkgs.writeShellScriptBin "make-vm" ''
-                  set -eux -o pipefail
-                  rm *.qcow2 || pass
-                  nix build .#nixosConfigurations.cloud.config.system.build.vm
-                  ./result/bin/run-cloud-vm
-                '';
-              in
-              with pkgs; [
-                make-vm
-                age
-                sops
-                inputs.deploy-rs.packages.${pkgs.system}.deploy-rs
-              ];
-          };
-        };
+          pkgs.mkShell {
+            packages = with pkgs; [sops age];
+          }
+      );
 
-      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) inputs.deploy-rs.lib;
+      pkgs = nixpkgs.lib.genAttrs supportedSystems (
+        system:
+          import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          }
+      );
     };
 }
--- modules/common.nix
+++ modules/common.nix
@@ -1,25 +1,19 @@
-{ config, pkgs, inputs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  lib,
+  ...
+}:
 {
   time.timeZone = "UTC";
   system.stateVersion = "21.11";
   system.configurationRevision = inputs.self.rev or null;
 
   environment.systemPackages = with pkgs; [
-    fup-repl
     fish
     htop
   ];
 
 
-  nix = {
-    package = pkgs.nixFlakes;
-
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-
-    generateNixPathFromInputs = true;
-    linkInputs = true;
-    generateRegistryFromInputs = true;
-  };
 }

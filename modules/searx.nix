934f0a203328f848ba2c6934525a197e7b84bc05
Fernando Ayats <ayatsfer@gmail.com>
2022-03-13 15:59:07 +0100

    searx tweaks

--- modules/searx.nix
+++ modules/searx.nix
@@ -5,6 +5,34 @@
   ...
 }: let
   localPort = 8001;
+  disabledEngines = [
+    "wikipedia"
+    "archive is"
+    "bing"
+    "currency"
+    "ddg definitions"
+    "erowid"
+    "wikidata"
+    "etools"
+    "etymonline"
+    "gigablast"
+    "library genesis"
+    "qwant"
+    "yahoo"
+    "wiby"
+    "wikibooks"
+    "wikiquote"
+    "wikisource"
+    "wiktionary"
+    "wikiversity"
+    "wikivoyage"
+    "dictzone"
+    "mymemory translated"
+    "duden"
+    "seznam"
+    "mojeek"
+    "naver"
+  ];
 in {
   services.searx = {
     enable = true;
@@ -18,29 +46,25 @@ in {
       search = {
         autocomplete = "duckduckgo";
       };
-      engines = lib.mkForce [
-        {
-          name = "google";
-          shotcut = "g";
-          engine = "google";
-        }
-        {
-          name = "brave";
-          shortcut = "brave";
-          engine = "xpath";
-          paging = false;
-          search_url = "https://search.brave.com/search?q={query}";
-          url_xpath = ''//div[@class="snippet fdb"]/a/@href'';
-          title_xpath = ''//span[@class="snippet-title"]'';
-          content_xpath = ''//p[1][@class="snippet-description"]'';
-          suggestion_xpath = ''/div[@class="text-gray h6"]/a'';
-          categories = "general";
-        }
-        {
-          name = "wikipedia";
-          disabled = true;
-        }
-      ];
+      engines = lib.mkForce ([
+          {
+            name = "brave";
+            shortcut = "brave";
+            engine = "xpath";
+            paging = false;
+            search_url = "https://search.brave.com/search?q={query}";
+            url_xpath = ''//div[@class="snippet fdb"]/a/@href'';
+            title_xpath = ''//span[@class="snippet-title"]'';
+            content_xpath = ''//p[1][@class="snippet-description"]'';
+            suggestion_xpath = ''/div[@class="text-gray h6"]/a'';
+            categories = "general";
+          }
+        ]
+        ++ (map (name: {
+            inherit name;
+            tokens = ["@SEARX_DUMMY_TOKEN@"];
+          })
+          disabledEngines));
     };
   };
 
--- overlay/default.nix
+++ overlay/default.nix
@@ -2,22 +2,22 @@ final: prev: let
   inherit (prev) callPackage fetchFromGitHub;
   my-httpx-socks = prev.python3Packages.callPackage ./httpx-socks.nix {};
 in {
-  searx = prev.searx.overrideAttrs (prevAttrs: {
-    version = "unstable-2022-03-05";
-    src = fetchFromGitHub {
-      owner = "searx";
-      repo = "searx";
-      rev = "f231d79a5ddd2ff211e401fe6ea2250325df116f";
-      sha256 = "06bxzfnvv3dncabp1z9qvy67y5n6drj6h9shsqaxzv5mzijqbn4s";
-    };
-    patches = [];
-    propagatedBuildInputs =
-      prevAttrs.propagatedBuildInputs
-      ++ (with final.python3Packages; [
-        uvloop
-        setproctitle
-        httpx
-        my-httpx-socks
-      ]);
-  });
+  # searx = prev.searx.overrideAttrs (prevAttrs: {
+  #   version = "unstable-2022-03-05";
+  #   src = fetchFromGitHub {
+  #     owner = "searx";
+  #     repo = "searx";
+  #     rev = "f231d79a5ddd2ff211e401fe6ea2250325df116f";
+  #     sha256 = "06bxzfnvv3dncabp1z9qvy67y5n6drj6h9shsqaxzv5mzijqbn4s";
+  #   };
+  #   patches = [];
+  #   propagatedBuildInputs =
+  #     prevAttrs.propagatedBuildInputs
+  #     ++ (with final.python3Packages; [
+  #       uvloop
+  #       setproctitle
+  #       httpx
+  #       my-httpx-socks
+  #     ]);
+  # });
 }
